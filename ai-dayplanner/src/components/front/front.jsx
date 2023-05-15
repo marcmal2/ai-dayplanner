62a67a276d1b71ee0f57891632e1388cb64b9f76
mmald <mmaldonadojr@hotmail.com>
2023-05-15 10:41:46 -0500

    added textfield and button

--- ai-dayplanner/src/components/front/front.jsx
+++ ai-dayplanner/src/components/front/front.jsx
@@ -4,8 +4,9 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
 
 const Front = () => {
   return (
@@ -23,6 +24,15 @@ const Front = () => {
             </Typography>
           </Toolbar>
         </AppBar>
+      </Box>
+      <Box m={2} pt={3} textAlign="center">
+        <TextField
+          id="outlined-basic"
+          Align="center"
+          label="Outlined"
+          variant="outlined"
+        />
+        <Button variant="contained">Send</Button>
       </Box>
     </>
   );
